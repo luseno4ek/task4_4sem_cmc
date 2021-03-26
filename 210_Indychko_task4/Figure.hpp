6f13115a2361676b01243abdfafde542da3c2ba8
luseno4ek <olindyv@mail.ru>
2021-03-26 19:10:01 +0300

    Initial Commit

---
+++ 210_Indychko_task4/Figure.hpp
@@ -0,0 +1,29 @@
+#ifndef Figure_hpp
+#define Figure_hpp
+
+#include <stdio.h>
+
+class Figure {
+protected:
+    double x_center;
+    double y_center;
+public:
+    Figure(double _x = 0, double _y = 0);
+    ~Figure();
+    virtual void Increase(double k) = 0;
+    virtual double GetSquare() = 0;
+};
+
+class Circle : public Figure {
+protected:
+    double radius;
+public:
+    Circle(double _x = 0, double _y = 0, double _rad = 1);
+    ~Circle();
+    virtual void Increase(double k);
+    virtual double GetSquare();
+};
+
+
+
+#endif
---
+++ 210_Indychko_task4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  210_Indychko_task4
+//
+//  Created by Олеся Индычко on 26.03.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
